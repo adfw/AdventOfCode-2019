8bd33da49306d144eb63703473b62e20271530cc
Anthony Williams <anthony@Precision-M6700>
2019-12-01 22:08:10 +0000

    Day 1; Part ii.

--- day1.adb
+++ day1.adb
@@ -2,6 +2,24 @@ with Ada.Text_IO;
 
 procedure Day1 is
 
+   function Mass_To_Fuel (Mass : Natural) return Natural is
+      ((Mass / 3) -2);
+
+   procedure Fuel_Calculation (Fuel : in out Natural)
+   is
+
+      Min_Fuel : constant Natural := 6;
+      Fuel_Remaining : Natural := Fuel;
+
+   begin
+
+      while Fuel_Remaining > Min_Fuel loop
+         Fuel_Remaining := Mass_To_Fuel(Fuel_Remaining);
+         Fuel := Fuel + Fuel_Remaining;
+      end loop;
+
+   end Fuel_Calculation;
+
    Input_Mass : Natural;
    Fuel       : Natural;
 
@@ -15,7 +33,11 @@ begin
       begin
 
          Input_Mass := Natural'Value(Line);
-         Fuel := (Input_Mass / 3) - 2; -- TODO: Underflow guard?
+         Fuel := Mass_To_Fuel(Input_Mass);
+
+         -- Next, deal with the fuel the fuel needs (and etc.)
+         Fuel_Calculation(Fuel);
+
          Accumulator := Accumulator + Fuel;
 
          exit when Ada.Text_IO.End_Of_File;
