bff21ed369e1330f604da76bfbaa3646f6af851c
Anthony Williams <anthony@Precision-M6700>
2020-05-06 22:31:48 +0100

    Tidy down to one file.

--- day3-reproducer/repro.adb
+++ day3-reproducer/repro.adb
@@ -1,13 +1,16 @@
+with Ada.Containers.Formal_Vectors;
 with Ada.Text_IO;
-
-with Coord;
 
 procedure Repro
    with SPARK_Mode => On
 is
 
+   package Points is new Ada.Containers.Formal_Vectors
+     (Element_Type => Natural,
+      Index_Type   => Natural);
+
    -- Instantiate Vec_2 to have a Capacity of 200.
-   Vec_2 : Coord.Points.Vector (200);
+   Vec_2 : Points.Vector (200);
 
    Pos : constant Natural := 12345;
 
@@ -15,11 +18,11 @@ begin
    
    -- Fails SPARK analysis, but we knew that was going to happen...
    for J in Positive range 1 .. 40005 loop
-      Coord.Points.Append(Vec_2, Pos);
-      Ada.Text_IO.Put_Line ("Vector Append" & Coord.Points.Length(Vec_2)'Image);
+      Points.Append(Vec_2, Pos);
+      Ada.Text_IO.Put_Line ("Vector Append" & Points.Length(Vec_2)'Image);
    end loop;
 
    Ada.Text_IO.Put_Line
-     ("Vector Finished" & Coord.Points.Length(Vec_2)'Image);
+     ("Vector Finished" & Points.Length(Vec_2)'Image);
 
 end Repro;
